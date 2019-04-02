a59aea3c2888948b4b09dc5386f7cddf79f29a17
Paul Vollmer <paul.vollmer@protonmail.com>
2019-04-02 21:34:01 +0200

    fixed generator scheme type, format and added description

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 test:
-	go build
+	@go fmt
+	@go build
 	./struct2oas -leftpad "  " -source fixtures/model1.go
 	./struct2oas -leftpad "  " -source fixtures/model2.go
+	./struct2oas -leftpad "  " -source fixtures/types.go
--- main.go
+++ main.go
@@ -14,10 +14,10 @@ import (
 )
 
 var (
-	flagSource = flag.String("source", "", "go source file")
+	flagSource  = flag.String("source", "", "go source file")
 	flagLeftpad = flag.String("leftpad", "", "left padding characters")
 	flagVersion = flag.Bool("version", false, "print the version and exit")
-	fset       *token.FileSet
+	fset        *token.FileSet
 )
 
 // Usage is a replacement usage function for the flags package.
@@ -82,9 +82,59 @@ func (g *Generator) Generate(t *ast.TypeSpec) {
 		for i := 0; i < len(s.Fields.List); i++ {
 			log.Printf("  Prop Name: %s\tType: %s\tTag: %s\n", s.Fields.List[i].Names[0], s.Fields.List[i].Type, s.Fields.List[i].Tag.Value)
 			g.Printf("    %s:\n", s.Fields.List[i].Names[0])
-			g.Printf("      type: %q\n", s.Fields.List[i].Type)
-			g.Printf("      description: %q\n", strings.Replace(s.Fields.List[i].Doc.Text(), "\n", "", -1))
-			// ast.Print(fset, s.Fields.List[i])
+
+			switch ty := s.Fields.List[i].Type.(type) {
+			case *ast.Ident:
+				typ, format := TypeToSchema(s.Fields.List[i].Type)
+				g.Printf("      type: %q\n", typ)
+				if format != "" {
+					g.Printf("      format: %q\n", format)
+				}
+				description := strings.Replace(s.Fields.List[i].Doc.Text(), "\n", "", -1)
+				if description != "" {
+					g.Printf("      description: %q\n", description)
+				}
+
+			case *ast.SelectorExpr:
+				if ty.X.(*ast.Ident).Name == "time" && ty.Sel.Name == "Time" {
+					g.Printf("      type: \"string\"\n")
+					g.Printf("      format: \"date-time\"\n")
+					description := strings.Replace(s.Fields.List[i].Doc.Text(), "\n", "", -1)
+					if description != "" {
+						g.Printf("      description: %q\n", description)
+					}
+				}
+
+			case *ast.ArrayType:
+				// log.Printf("----------- TODO %+v\n", ty.(*ast.ArrayType).Name)
+				// ast.Print(fset, ty)
+				// log.Println("TYYYYY", ty, TypeToSchema(s.Fields.List[i].Type))
+
+				g.Printf("      type: \"array\"\n")
+				g.Printf("      items:\n")
+				typ, format := TypeToSchema(ty.Elt)
+				g.Printf("        type: %q\n", typ)
+				if format != "" {
+					g.Printf("        format: %q\n", format)
+				}
+				// g.Printf("        type: %q\n", ty.Elt.(*ast.Ident).Name)
+				// g.Printf("        properties:\n")
+
+			case *ast.MapType:
+				g.Printf("      type: \"object\"\n")
+				description := strings.Replace(s.Fields.List[i].Doc.Text(), "\n", "", -1)
+				if description != "" {
+					g.Printf("      description: %q\n", description)
+				}
+
+				// case *ast.StructType:
+				// 	g.Printf("      type: \"object\"\n")
+				// 	description := strings.Replace(s.Fields.List[i].Doc.Text(), "\n", "", -1)
+				// 	if description != "" {
+				// 		g.Printf("      description: %q\n", description)
+				// 	}
+			}
+			// ast.Print(fset, s.Fields.List[i].Type)
 		}
 	}
 }
@@ -94,4 +144,38 @@ func (g *Generator) WriteFile() {
 	if err != nil {
 		log.Fatalf("writing output: %s", err)
 	}
+}
+
+func TypeToSchema(e ast.Expr) (t string, f string) {
+	switch e.(*ast.Ident).Name {
+	case "string":
+		t = "string"
+		f = ""
+		break
+	case "bool":
+		t = "boolean"
+		f = ""
+		break
+	case "int", "int8", "int16", "uint", "uint8", "uint16", "byte", "rune":
+		t = "integer"
+		f = ""
+		break
+	case "int32", "uint32":
+		t = "integer"
+		f = "int32"
+		break
+	case "int64", "uint64":
+		t = "integer"
+		f = "int64"
+		break
+	case "float32":
+		t = "number"
+		f = "float"
+		break
+	case "float64", "complex64", "complex128":
+		t = "number"
+		f = "double"
+		break
+	}
+	return
 }
